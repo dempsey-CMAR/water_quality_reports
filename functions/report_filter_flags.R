fdfb871ecc6dd44b29317903c19cff845b854762
Danielle Dempsey <ddempsey@perennia.ca>
2025-12-16 16:19:05 -0400

    hard code flag values to keep for DO

--- functions/report_filter_flags.R
+++ functions/report_filter_flags.R
@@ -3,11 +3,6 @@
 #' @param dat Data frame with summary quality control flags in long format
 #'   (e.g., output of `qc_pivot_longer()`).
 #'
-#' @param keep_dissolved_oxygen_percent_saturation Vector of acceptable flags
-#'   for variable dissolved_oxygen_percent_saturation. Default is c(1, 2, NA).
-#'   "Suspect/Of Interest" observations are considered biofouling and excluded
-#'   from analysis.
-#'
 #' @param keep_dissolved_oxygen_uncorrected_mg_per_l Vector of acceptable flags
 #'   for variable dissolved_oxygen_uncorrected_mg_per_l. Default is c(1, 2, NA).
 #'   "Suspect/Of Interest" observations are considered biofouling and excluded
@@ -37,9 +32,8 @@
 
 report_filter_flags <- function(
     dat,
-
    # keep_dissolved_oxygen_percent_saturation = c(1, NA),
-    keep_dissolved_oxygen_uncorrected_mg_per_l = c(1, NA),
+   # keep_dissolved_oxygen_uncorrected_mg_per_l = c(1, NA),
     keep_salinity_psu = c(1, NA),
     keep_sensor_depth_measured_m = c(1, 3, NA),
     keep_temperature_degree_c = c(1, 3, NA),
@@ -59,7 +53,7 @@ report_filter_flags <- function(
   #   )
   # }
 
-  dat %>%
+  dat <- dat %>%
     filter(
       # (variable == "dissolved_oxygen_percent_saturation" &
       #    station %in% keep_sus_do_stations & qc_flag_value %in% keep_sus_do) |
@@ -67,23 +61,38 @@ report_filter_flags <- function(
         (variable == "dissolved_oxygen_percent_saturation" &
            grossrange_flag_value %in% c(1, 3, NA) &
            climatology_flag_value %in% c(1, 3, NA) &
-           spike_flag_value %in% c(1, NA) &
+           spike_flag_value %in% c(1, 3, NA) &
            rolling_sd_flag_value %in% c(1, NA)
          ) |
 
-        (variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-           qc_flag_value %in% keep_dissolved_oxygen_uncorrected_mg_per_l) |
+        # (variable == "dissolved_oxygen_uncorrected_mg_per_l" &
+        #    qc_flag_value %in% keep_dissolved_oxygen_uncorrected_mg_per_l) |
+
+          (variable == "dissolved_oxygen_uncorrected_mg_per_l" &
+             grossrange_flag_value %in% c(1, 3, NA) &
+             climatology_flag_value %in% c(1, 3, NA) &
+             spike_flag_value %in% c(1, 3, NA) &
+             rolling_sd_flag_value %in% c(1, NA)
+          ) |
 
         (variable == "sensor_depth_measured_m" &
            qc_flag_value %in% keep_sensor_depth_measured_m) |
 
         (variable == "salinity_psu" & qc_flag_value %in% keep_salinity_psu) |
 
-        (variable == "temperature_degree_c" &
-           qc_flag_value %in% keep_temperature_degree_c)
+          (variable == "temperature_degree_c" &
+             qc_flag_value %in% keep_temperature_degree_c)
     )
 
+  # because human_in_loop flags not pivoted
+  if("human_in_loop_flag_dissolved_oxygen_percent_saturation" %in% colnames(dat)) {
 
+    dat <- dat %>%
+      filter(
+        human_in_loop_flag_dissolved_oxygen_percent_saturation %in% c(1, NA))
+  }
+
+  dat
 }
 
 
